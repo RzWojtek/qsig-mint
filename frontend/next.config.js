0e0d4689ff6cf6415dc87974748745ea87b628ac
RzWojtek <116317334+RzWojtek@users.noreply.github.com>
2026-05-12 02:18:01 +0200

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://185.202.239.239:8080/api/:path*',
+      },
+    ]
+  },
+}
 module.exports = nextConfig
